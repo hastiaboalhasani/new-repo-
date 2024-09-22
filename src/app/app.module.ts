6b1a6e36ab73cb70cda5216b5665b0144258b39c
hastiaboalhasani <aboalhasanihasti@gmail.com>
2024-09-22 13:56:10 +0330

    FEAT

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CourseCardComponent } from './course-card/course-card.component';
 import { CourseImageComponent } from './course-image/course-image.component';
+import { HighlightedDirective } from './directives/highlighted.directive';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,11 @@ import { CourseImageComponent } from './course-image/course-image.component';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HighlightedDirective,
+  ],
+  exports: [
+    HighlightedDirective
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/directives/highlighted.directive.ts
+++ src/app/directives/highlighted.directive.ts
@@ -1,14 +1,36 @@
-import { Directive, HostBinding } from '@angular/core';
+import { Directive, HostBinding, HostListener, Input, Output } from "@angular/core";
+import EventEmitter from "events";
 
 @Directive({
-  selector: '[highlighted]',
-  standalone: true
+  selector: "[highlighted]",
+  standalone: true,
 })
 export class HighlightedDirective {
+  @Input("highlighted")
+  isHighlighted = false;
 
+  @Output()
+  toggleHighlight = new EventEmitter();
   constructor() {
-    console.log("directive created..");
+    
   }
-  
+  @HostBinding("class.highlighted")
+  get cssClasses() {
+    return this.isHighlighted 
+  }
 
+  @HostBinding('attr.disabled')
+  get  disabled(){
+    return 'true';
+  }
+  @HostListener('mouseover')
+  mouseOver(){
+    this.isHighlighted = true;
+    this.toggleHighlight.emit(this.isHighlighted);
+  }
+  @HostListener('mouseleave')
+  mouseLeave(){
+    this.isHighlighted = false;
+    this.toggleHighlight.emit(this.isHighlighted);
+  }
 }
